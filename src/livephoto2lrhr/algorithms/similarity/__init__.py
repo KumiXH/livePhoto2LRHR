d2497bbdd226cf00736b5772b0d153047c3bddb7
KumiXH <xhk932432553@gmail.com>
2026-06-13 00:10:54 +0800

    feat: run frame selection pipeline

--- src/livephoto2lrhr/algorithms/similarity/__init__.py
+++ src/livephoto2lrhr/algorithms/similarity/__init__.py
@@ -1,1 +1,11 @@
+from livephoto2lrhr.algorithms.similarity.base import FrameSelector
+from livephoto2lrhr.algorithms.similarity.fake import FakeFrameSelector
+from livephoto2lrhr.algorithms.similarity.opencv import OpenCVSimilaritySelector
+from livephoto2lrhr.pipeline.registry import Registry
 
+
+def build_similarity_registry() -> Registry[FrameSelector]:
+    registry: Registry[FrameSelector] = Registry()
+    registry.register("fake_selector", FakeFrameSelector)
+    registry.register("opencv_similarity", OpenCVSimilaritySelector)
+    return registry
